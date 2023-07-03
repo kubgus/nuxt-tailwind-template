faf9e4b21591561e237f9d8662e100489300cc91
kubgus <ja.gustafik@outlook.sk>
2023-07-04 01:30:42 +0200

    Strip excessive features

--- error.vue
+++ error.vue
@@ -1,20 +1,17 @@
 <template>
-    <div class="text-center">
+    <div>
         <Head>
             <Title>Error {{ error.statusCode }}</Title>
             <Meta name="description" :content="error.message" />
         </Head>
 
-        <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
-        <p class="mt-7 text-xl">{{ error.message }}</p>
-        <button @click="handleClearError" class="mt-7 underline">Go back</button>
+        <!-- ... -->
     </div>
 </template>
 
 <script setup>
     defineProps(["error"]);
-
-    const handleClearError = () => clearError({ redirect: "/"});
+    // https://nuxt.com/docs/api/utils/clear-error
 </script>
 
 <style lang="scss" scoped>
